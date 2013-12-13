021fe461d830168ce51dffae6367a4f3648ace69
christophernorman <normtronics@hotmail.com>
2013-12-12 19:50:41 -0500

    First Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+BlueAPI2::Application.config.secret_key_base = '24910d35eb84c80b50c06cef1edb565a20b02821ca1efd99a669b04dc311bc849c6a57f19433945447ea2878d694fc4dda99030a4c1a2993614aacf7bd6e9fbc'
